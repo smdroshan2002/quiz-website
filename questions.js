5a7d60dc5b1bb4405039a98217fa901b66bb323e
Shaik Mohammed Roshan <148673311+smdroshan4@users.noreply.github.com>
2024-06-11 10:48:57 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb:1,
+        question:"What does HTML stands for?",
+        answer:"C. Hyper Text Markup Language",
+        options:[
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Hyper Text Multi Language"
+        ]
+    },
+    {
+        numb:2,
+        question:"What does CSS stands for?",
+        answer:"A. Cascading Style Sheets",
+        options:[
+            "A. Cascading Style Sheets",
+            "B. Cute Style Sheets",
+            "C. Computer Style Sheets",
+            "D. Case Sensitive Style Sheets"
+        ]
+    },
+    {
+        numb:3,
+        question:"What does PHP stands for?",
+        answer:"A. HyperText Preprocessor",
+        options:[
+            "A. HyperText Preprocessor",
+            "B. HomeText Programming",
+            "C. HyperText Programming",
+            "D. Programming HyperText Preprocessor"
+        ]
+    },
+    {
+        numb:4,
+        question:"What does SQL Stands for?",
+        answer:"B. Structured Query Language",
+        options:[
+            "A. Strength Query Language",
+            "B. Structured Query Language",
+            "C. Science Question Language",
+            "D. StyleSheet Query Language"   
+        ]
+    },
+    {
+        numb:5,
+        question:"What does XML stands for",
+        answer:"C. Extensible Markup Language",
+        options:[
+            "A. Excellent Markup Language",
+            "B. Explore Markup Language",
+            "C. Extensible Markup Language",
+            "D. Extra Markup Language"
+        ]
+    }
+];
